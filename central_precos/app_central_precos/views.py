74ef864897d61094c96329a1cbcefda3b4f9e5e4
RobsonBSF <robsonbsfilho@gmail.com>
2025-03-25 20:40:31 -0300

    inicio front-end

--- central_precos/app_central_precos/views.py
+++ central_precos/app_central_precos/views.py
@@ -3,9 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view (request):
-    context = {
-        'titulo': 'Título Teste',
-        'mensagem': 'teste view'
-    }
-    
-    return render(request, 'base.html', context)
+    return render(request, 'base.html')
